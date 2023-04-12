7e7e09c55b361b4646eb3382e26d7f6e820a015d
Voronovs <vitalijs.voronovs@accenture.com>
2023-04-12 17:54:04 +0300

    security filter turn on

--- src/main/java/com/paymentology/weather/security/ApiKeyAuthFilter.java
+++ src/main/java/com/paymentology/weather/security/ApiKeyAuthFilter.java
@@ -12,7 +12,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.Optional;
 
-//@Component
+@Component
 @RequiredArgsConstructor
 public class ApiKeyAuthFilter extends OncePerRequestFilter {
 
