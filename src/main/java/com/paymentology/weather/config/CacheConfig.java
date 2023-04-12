1a47f1efc3fd36591612d363e980f1e3d02feb0f
Voronovs <vitalijs.voronovs@accenture.com>
2023-04-12 19:49:12 +0300

    add cache

--- src/main/java/com/paymentology/weather/config/CacheConfig.java
+++ src/main/java/com/paymentology/weather/config/CacheConfig.java
@@ -1,46 +1,21 @@
 package com.paymentology.weather.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 
-import java.time.Duration;
+import java.util.Arrays;
 
 @Configuration
 @EnableCaching
 @RequiredArgsConstructor
-public class CacheConfig {
+public class CacheConfig implements CacheManagerCustomizer<ConcurrentMapCacheManager> {
 
-    @Bean
-    public RedisCacheConfiguration cacheConfiguration() {
-        var serializationPair = SerializationPair
-                .fromSerializer(new GenericJackson2JsonRedisSerializer());
-
-        return RedisCacheConfiguration
-                .defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(120))
-                .disableCachingNullValues()
-                .serializeValuesWith(serializationPair);
-    }
-
-    @Bean
-    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
-        var locationConfig = RedisCacheConfiguration
-                .defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(60));
-
-        var weatherConfig = RedisCacheConfiguration
-                .defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(60));
-
-        return builder -> builder
-                .withCacheConfiguration("geoLocationCache", locationConfig)
-                .withCacheConfiguration("weatherCache", weatherConfig);
+    @Override
+    public void customize(ConcurrentMapCacheManager cacheManager) {
+        cacheManager.setCacheNames(Arrays.asList("geoLocationCache", "weatherCache"));
     }
 
 }
