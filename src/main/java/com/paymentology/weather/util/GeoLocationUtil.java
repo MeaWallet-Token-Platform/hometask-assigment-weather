50f264885b1961cebf3899247e04fdbaec0cf446
Voronovs <vitalijs.voronovs@accenture.com>
2023-04-18 15:40:27 +0300

    delete HostDto

--- src/main/java/com/paymentology/weather/util/GeoLocationUtil.java
+++ src/main/java/com/paymentology/weather/util/GeoLocationUtil.java
@@ -12,6 +12,7 @@ public class GeoLocationUtil {
     private static final double STANDARD_LATITUDE = 25.761681;
     private static final double STANDARD_LONGITUDE = -80.191788;
 
+
     public GeoLocationDto getStandardLocation(String host) {
         return new GeoLocationDto(host, STANDARD_LATITUDE, STANDARD_LONGITUDE);
     }
