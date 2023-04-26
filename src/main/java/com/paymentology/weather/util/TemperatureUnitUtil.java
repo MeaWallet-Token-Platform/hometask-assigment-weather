488875379d2a9d6dd18c550146c79a885495f072
Voronovs <vitalijs.voronovs@accenture.com>
2023-04-26 09:56:52 +0300

    refactor switch to log if no valid value was provided

--- src/main/java/com/paymentology/weather/util/TemperatureUnitUtil.java
+++ src/main/java/com/paymentology/weather/util/TemperatureUnitUtil.java
@@ -1,6 +1,7 @@
 package com.paymentology.weather.util;
 
 import com.paymentology.weather.constant.TemperatureUnit;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import static com.paymentology.weather.constant.TemperatureUnit.CELSIUS;
@@ -8,14 +9,21 @@ import static com.paymentology.weather.constant.TemperatureUnit.FAHRENHEIT;
 import static com.paymentology.weather.constant.TemperatureUnit.KELVIN;
 
 @Component
+@Slf4j
 public class TemperatureUnitUtil {
+
+    public static final String NO_VALUE_SUBSTITUTING = "No value found for provided TemperatureUnit: %s, substituting with CELSIUS";
 
     public TemperatureUnit determineUnit(String unitString) {
 
         return switch (unitString) {
             case String unit && KELVIN.toString().equalsIgnoreCase(unit) -> KELVIN;
             case String unit && FAHRENHEIT.toString().equalsIgnoreCase(unit) -> FAHRENHEIT;
-            case null, default -> CELSIUS;
+            case String unit && CELSIUS.toString().equalsIgnoreCase(unit) -> CELSIUS;
+            case null, default -> {
+                log.info(NO_VALUE_SUBSTITUTING.formatted(unitString));
+                yield CELSIUS;
+            }
         };
 
     }
